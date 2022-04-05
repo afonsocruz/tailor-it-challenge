bfc62392f48c4131db77712cf8add9e2dbaa10b7
unknown <afonsocruz.rj@gmail.com>
2022-04-04 22:21:11 -0300

    feat: globalstyle and global fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { GlobalStyle } from './styles/GlobalStyle';
+
 const App: React.FC = () => {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <p>Tailor IT - Front end challenge Bookstore</p>
+    </>
+  );
 };
 
 export default App;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-family: 'Oswald', sans-serif;
+  }
+`;
